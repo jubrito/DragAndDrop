fd64b64dc32a0a467039bd464cb5f2e5f8a651e5
Juliana Witzke de Brito <=>
2024-03-14 16:23:07 -0300

    adds optional parameters

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,21 @@
 "use strict";
+let add;
+add = (n1, n2) => {
+    return n1 + n2;
+};
 class Person {
     constructor(name, age) {
         this.name = name;
         this.age = age;
+        if (age) {
+            this.age = age;
+        }
     }
     greet(phrase) {
         console.log(phrase + ' ' + this.name);
+        if (this.age) {
+            console.log("My age is " + this.age);
+        }
     }
 }
 let user1;
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,40 @@
+type AddFn = (a: number, b: number) => number;
+interface AddFnAsInterface {
+    (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+    return n1 + n2
+}
+
+interface Named {
+    readonly name: string;
+    outputName?: string;
+}
+
 // describes the structure of an object
-interface Greetable {
-    name: string;
+interface Greetable extends Named {
     greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-    constructor(public name: string, public age: number) {}
+    constructor(public name: string, public age?: number) {
+        if (age) {
+            this.age = age;
+        }
+    }
 
     greet(phrase: string) {
         console.log(phrase+ ' ' + this.name)
+        if (this.age) {
+            console.log("My age is "+this.age)
+        }
     }
 }
 
-let user1: Person
+let user1: Greetable;
 user1 = new Person("Juliana", 26);
 user1.greet('Hi, my name is')
 console.log(user1);
