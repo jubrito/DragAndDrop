c67ce3a0495013ba54ff1270ccfdf2d1b8602693
Juliana Witzke de Brito <=>
2024-03-15 17:04:29 -0300

    uses generics functions

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
 "use strict";
+const names = ['Ju', 'Bel', 'Gabi'];
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is done!');
+    }, 200);
+    reject('Rejected');
+});
+promise.then(data => {
+    data.split(' ');
+});
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Ju', hobbies: ['lol'] }, { age: 26 });
+console.log(mergedObj);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 const names: Array<string> = ['Ju', 'Bel', 'Gabi'];
-
 const promise: Promise<string> = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('This is done!')
     }, 200)
+    reject('Rejected');
 });
 
 promise.then(data => {
     data.split(' ');
 })
+
+function merge<T extends object, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({name: 'Ju', hobbies: ['lol']}, {age: 26})
+console.log(mergedObj)
