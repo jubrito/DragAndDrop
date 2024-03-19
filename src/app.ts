02315138c3341d3c61b88a9e7ffc8d79ccdcbf44
Juliana Witzke de Brito <=>
2024-03-19 12:46:41 -0300

    use constrains on generics

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ promise.then(data => {
     data.split(' ');
 })
 
-function merge<T extends object, U>(objA: T, objB: U) {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA, objB);
 }
 const mergedObj = merge({name: 'Ju', hobbies: ['lol']}, {age: 26})
