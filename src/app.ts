346c547abbac6bffc725b15299421326892c6af6
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-03-28 15:50:29 -0300

    uses declare as a last resort

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
-import { ProjectInput } from "./DragAndDrop/components/project-input";
-import { ProjectList } from "./DragAndDrop/components/project-list";
-new ProjectInput();
-new ProjectList('active');
-new ProjectList('finished');
+// DRAG AND DROP
+// import { ProjectInput } from "./DragAndDrop/components/project-input";
+// import { ProjectList } from "./DragAndDrop/components/project-list";
+// document.querySelector('h1')!.innerHTML = 'Project Manager';
+// new ProjectInput();
+// new ProjectList('active');
+// new ProjectList('finished');
+
+import _ from 'lodash';
+declare var GLOBAL: string;
+
+console.log(_.shuffle([1,2,3,4]))
+console.log(GLOBAL);
