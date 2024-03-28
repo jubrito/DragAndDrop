febd05b5a45c8510a50fbb052430587f6eb5770f
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-03-28 15:14:06 -0300

    adds the webpack dev server and creates a development workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development', // fewer optimizations, make debugging easier and more meaningful error messages
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist') // requires an absolute path
+        path: path.resolve(__dirname, 'dist'), // requires an absolute path
+        publicPath: '/dist/'
     },
     devtool: 'inline-source-map', // there will be generated sourcemaps already which we should extract and wire up correctly to the bundle it generates 
     module: { // how to work with files and imports
@@ -18,5 +20,12 @@ module.exports = {
     },
     resolve: { // which file extensions it adds to the imports it finds
         extensions: ['.ts', '.js'] // bundle together all files we are importing that have these extensions 
-    }
+    },
+    devServer: {
+        static: [
+            {
+                directory: path.join(__dirname),
+            },
+        ],
+    },
 }
