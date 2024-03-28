035d0b6856e0f9f4101d09eaf59662d100781216
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-03-28 15:07:58 -0300

    adds typescript support with the ts-loader package

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
-const path = requre('path');
+const path = require('path');
 
 module.exports = {
     entry: './src/app.ts',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist') // requires an absolute path
+    },
+    devtool: 'inline-source-map', // there will be generated sourcemaps already which we should extract and wire up correctly to the bundle it generates 
+    module: { // how to work with files and imports
+        rules: [
+            {
+                test: /\.ts$/,  // test on every file to see if the rules applies or not
+                use: 'ts-loader', // any typescript file should be handled by ts-loader
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: { // which file extensions it adds to the imports it finds
+        extensions: ['.ts', '.js'] // bundle together all files we are importing that have these extensions 
     }
 }
