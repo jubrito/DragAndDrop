f05417a18f5ae74781c957b6a897356e09e3daf0
Juliana Witzke de Brito <jubrito@alumni.usp.br>
2024-04-01 17:43:30 -0300

    reorganize project structure

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
-// DRAG AND DROP
-// import { ProjectInput } from "./DragAndDrop/components/project-input";
-// import { ProjectList } from "./DragAndDrop/components/project-list";
-// document.querySelector('h1')!.innerHTML = 'Project Manager';
-// new ProjectInput();
-// new ProjectList('active');
-// new ProjectList('finished');
-
-import _ from 'lodash';
-declare var GLOBAL: string;
-
-console.log(_.shuffle([1,2,3,4]))
-console.log(GLOBAL);
+// Move this code to the main app.ts to access the DRAG AND DROP
+import { ProjectInput } from "./components/project-input";
+import { ProjectList } from "./components/project-list";
+document.querySelector('h1')!.innerHTML = 'Project Manager';
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
